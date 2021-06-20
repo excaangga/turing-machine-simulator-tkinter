91fd3ad277e2624ec184892e82636f30ee39b961
adityaazizi <adityaazizi@student.uns.ac.id>
2021-06-20 19:46:28 +0700

    pengurangan done

--- substractionTM.py
+++ substractionTM.py
@@ -23,42 +23,42 @@ for char in inputString:
 state = 0
 oldHead = -1
 acc = False
-X, R, L, B = 'X', 'R', 'L', 'B'  # simbol yang diperlukan fungsi
-s = 's'  # simbol adisi
+X, Y, R, L, B = 'X', 'Y', 'R', 'L', 'B' #simbol yang diperlukan fungsi
+
 while(oldHead != head):
     oldHead = head
     print(tape, ", head di index ", head, " pada state ", state)
-
+    
     if state == 0:
-        if action('0', '0', R, 0) or action(s, s, R, 1):
+        if action('0', '0', R, 0) or action(X, X, R, 1):
             pass
-
+    
     elif state == 1:
-        if action(X, X, R, 1) or action(0, X, L, 2) or action(B, B, L, 5):
+        if action('0', '0', R, 1) or action(B, B, L, 2):
             pass
-
+    
     elif state == 2:
-        if action(X, X, L, 2) or action(s, s, L, 3):
+        if action('0', B, L, 3) or action(X, B, L,6):
             pass
-
+    
     elif state == 3:
-        if action('0', '0', L, 3) or action(B, B, R, 4):
+        if action('0', '0', L, 3) or action(X, X, L, 4):
             pass
-
+    
     elif state == 4:
-        if action('0', B, R, 0):
+        if action('0', '0', L, 4) or action(B, B, R, 5):
             pass
-
+    
     elif state == 5:
-        if action(X, B, L, 5) or action(s, B, L, 6):
+        if action('0', B, R, 0):
             pass
 
     elif state == 6:
         acc = True
 
+
 elements_count = collections.Counter(tape)
 if acc:
-    print("Input halt dan diterima di state: ", state,
-          " dengan hasil: ", elements_count['0'])
+    print("Input halt dan diterima di state: ", state, " dengan hasil: ", elements_count['0'])
 else:
-    print("Input tidak diterima di state: ", state)
+    print("Input tidak diterima di state: ", state)    
