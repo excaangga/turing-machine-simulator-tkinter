4a90ecca8750d5892154d3672a5cd2787f940346
Angga Exca P.S <excaangga@gmail.com>
2021-06-20 13:57:34 +0700

    adding: proper commands, division to GUI
    ggwp

--- gui.py
+++ gui.py
@@ -1,15 +1,16 @@
 from tkinter import *
 from tkinter import ttk
 import collections
-import os
 
-
+# this block is for restarting the operation
 def restart():
     canvas.delete("all")
     for widget in frameResult.winfo_children():
         widget.pack_forget()
 
 ##################################################################
+
+# this block handles the turing tape movement
 def action(inputSymbol, inputReplace, movement, nextState):
     global head, state, tape
     if tape[head] == inputSymbol:
@@ -21,97 +22,105 @@ def action(inputSymbol, inputReplace, movement, nextState):
             head -= 1
         return True
     return False
+
 ##################################################################
 
+# this is basically the window creation
 window = Tk()
 window.title("Turing Machine Simulator")
 window.geometry('700x700')
 
+# titling the window
 title = Label(window, text="Turing Machine Simulator", width=700, anchor=N, background="#3caea3")
 title.config(font=("Roboto", 20), foreground="white", padx=10, pady=10)
 title.pack(pady=(0, 20))
 
-# separator = ttk.Separator(orient='horizontal')
-# separator.place(x=0, y=50, relwidth=1, relheight=1)
-
+# make a frame that contains input widgets, later called as "INPUT" in its child comments
 frameInput = ttk.LabelFrame(window, text="input")
 frameInput.pack(padx=10, pady=10)
 
+# INPUT | entry1(input1)
+# this is the param
 input1 = StringVar(window)
 input1.set("positive integer")
 
+# INPUT | option(operand)
+# this is the param
 operand = StringVar(window)
 operand.set("+")
 
+# INPUT | entry2(input2)
+# this is the param
 input2 = StringVar(window)
 input2.set("positive integer")
 
+# INPUT | entry1(input1)
 entry1 = ttk.Entry(frameInput, textvariable=input1)
 entry1.pack(padx=20, pady=20, side=LEFT, anchor=CENTER)
 
+# INPUT | option(operand)
 option = ttk.OptionMenu(frameInput, operand,"+", "-", "*", "/", "!", "%", "^", "log")
 option.pack(padx=20, pady=20, side=LEFT, anchor=CENTER)
 
+# INPUT | entry2(input2)
 entry2 = ttk.Entry(frameInput, textvariable=input2)
 entry2.pack(padx=20, pady=20, side=LEFT, anchor=CENTER)
 
-
-
+# make a frame that contains output drawings
 frameOutput = ttk.LabelFrame(window, text="output", width=600)
 
+# defining the scrollbar, both vertical and horizontal
 v = Scrollbar(frameOutput)
 h = Scrollbar(frameOutput, orient=HORIZONTAL)
 v.pack(side=RIGHT, fill=Y)
 h.pack(side=BOTTOM, fill=X)
+
+# making canvas for the tapes' drawings
 canvas = Canvas(frameOutput, width=550, height=400, yscrollcommand=v.set, xscrollcommand=h.set)
 canvas.pack()
+
+# assigning the scrollbar to the canvas
 v.config(command=canvas.yview)
 h.config(command=canvas.xview)
 
 frameOutput.pack()
 
-
-
+# make a frame that contains result of the calculation, later called as "RESULT" in its child comments
 frameResult = Frame(window)
 frameResult.pack(anchor=CENTER)
 
-
-    
-
 ##################################################################
 
+# the function that responsible for drawing tapes
 def drawInline(inputLength, x1, x2, y1, y2, counter, tape, head):
     for j in range (inputLength):
         x1 += 20
         x2 += 20
-        if True:
-            box = canvas.create_rectangle(x1, y1, x2+20, y2, fill="white smoke")
-            #print("TESTTTTT", j)
-            cells.append(box)
-            label = canvas.create_text((x1+x2)/2 + 10, (y1+y2)/2, text=tape[j])
-            if head == j:
-                canvas.itemconfig(box, fill="yellow")
-            
-            canvas.config(scrollregion=(0, 0, x1+40, y1+40))
-            canvas.pack(expand=YES, fill=BOTH)
-            counter += 1
+        box = canvas.create_rectangle(x1, y1, x2+20, y2, fill="white smoke")
+        label = canvas.create_text((x1+x2)/2 + 10, (y1+y2)/2, text=tape[j])
+        if head == j:
+            canvas.itemconfig(box, fill="yellow")
         
-        # else:
-        #     drawInline(inputLength-counter, 0, 0, y1+40, y2+40, counter, tape)
-
+        canvas.config(scrollregion=(0, 0, x1+40, y1+40))
+        canvas.pack(expand=YES, fill=BOTH)
+        counter += 1
+        
 ##################################################################
 
+# main function for starting the turing machine
 def caller():
     global temp1, temp2, head, state, tape, cells
     temp1, temp2 = "", ""
-    cells = []
+    
+    # converting GUI input to CLI input
     for i in range (int(input1.get())):
         temp1 += "0"
     for i in range (int(input2.get())):
         temp2 += "0"
+
+    # + operation
     if operand.get() == "+":
-        inputAdd = temp1 + "a" + temp2
-        inputString = inputAdd
+        inputString = temp1 + "a" + temp2
         inputLength = len(inputString) * 2
         tape = ['B'] * inputLength
         i = 1
@@ -124,9 +133,12 @@ def caller():
         state = 0
         oldHead = -1
         acc = False
-        X, R, L, B = 'X', 'R', 'L', 'B' #simbol yang diperlukan fungsi
-        a = 'a' #simbol adisi
+        # just "as-usual" turing symbols
+        X, R, L, B = 'X', 'R', 'L', 'B'
+        # symbol for addition
+        a = 'a'
         increment = 0
+        # a whole movement block
         while(oldHead != head):
             oldHead = head
             print(tape, ", head di index ", head, " pada state ", state)
@@ -156,34 +168,43 @@ def caller():
                 if action(X, B, L, 5):
                     acc = True
                     pass
-
+        
+        # make a counter for the 0's in the tape as the final result
         elements_count = collections.Counter(tape)
         if acc:
             print("Input halt dan diterima di state: ", state, " dengan hasil: ", elements_count['0'])
+            # RESULT | labels
             ttk.Label(frameResult, text="Result: ").pack(pady=10)
             ttk.Label(frameResult, text=elements_count['0']).pack()
         else:
             print("Input tidak diterima di state: ", state)    
 
+    # / operation
     elif operand.get() == "/":
-        inputDiv = temp1 + "d" + temp2
-        inputString = inputDiv
+        inputString = temp1 + "d" + temp2
         inputLength = len(inputString) * 2
         tape = ['B'] * inputLength
         i = 1
         head = 1
+        x1, x2 = 0, 0
+        y1, y2 = 20, 40
         for char in inputString:
             tape[i] = char
             i += 1
         state = 0
         oldHead = -1
         acc = False
-        X, Y, R, L, B = 'X', 'Y', 'R', 'L', 'B' #simbol yang diperlukan fungsi
-        d = 'd' #simbol divisi
+        # just "as-usual" turing symbols
+        X, Y, R, L, B = 'X', 'Y', 'R', 'L', 'B'
+        # symbol for division
+        d = 'd'
+        increment = 0
+        # a whole movement block
         while(oldHead != head):
             oldHead = head
             print(tape, ", head di index ", head, " pada state ", state)
-
+            drawInline(inputLength, x1, x2, y1+increment, y2+increment, 0, tape, head)
+            increment += 40
             if state == 0:
                 if action('0', B, R, 1) or action(d, B, R, 8):
                     pass
@@ -223,9 +244,13 @@ def caller():
             elif state == 9:
                 acc = True
 
+        # make a counter for the 0's in the tape as the final result
         elements_count = collections.Counter(tape)
         if acc:
             print("Input halt dan diterima di state: ", state, " dengan hasil: ", elements_count['0'])
+            # RESULT | labels
+            ttk.Label(frameResult, text="Result: ").pack(pady=10)
+            ttk.Label(frameResult, text=elements_count['0']).pack()
         else:
             print("Input tidak diterima di state: ", state)    
 
